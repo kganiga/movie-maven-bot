06f5adb4f7646b6abf23ee6437820495544d6ad8
Khalil <khalil.b.ganiga@outlook.com>
2024-07-17 23:06:57 +0530

    updated env variable

--- pages/api/bot.js
+++ pages/api/bot.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "dotenv/config";
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: false });
-bot.setWebHook(`${process.env.VERCEL_URL}/api/bot`);
+bot.setWebHook(`${process.env.VERCEL_PUBLIC_URL}/api/bot`);
 
 const locale = "en-IN";
 const country = locale.split("-")[1].toUpperCase();
